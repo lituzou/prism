0b3e0125fb3a4e7ad439d1ed5880a11fdf9e924b
Litu Zou <litu.zou@outlook.com>
2025-12-29 16:10:34 +0000

    Update cudd.cmake

--- prism/ext/cudd.cmake
+++ prism/ext/cudd.cmake
@@ -1,35 +1,35 @@
 set(CUDD_ROOT "${CMAKE_CURRENT_SOURCE_DIR}/../../cudd")
 set(CUDD_INSTALL_DIR "${CMAKE_CURRENT_BINARY_DIR}/cudd")
+set(CUDD_BINARY_DIR "${CMAKE_CURRENT_BINARY_DIR}/cudd-build")
+
+find_program(MAKE_EXECUTABLE NAMES gmake make mingw32-make REQUIRED)
+
+set(CUDD_LIB "${CUDD_INSTALL_DIR}/lib/libcudd.a")
 
 ExternalProject_Add(cudd_src
     PREFIX ${CUDD_INSTALL_DIR}
     SOURCE_DIR ${CUDD_ROOT}
+    BINARY_DIR ${CUDD_BINARY_DIR}
     # Avoid autoconf issue by changing time-stamp
     PATCH_COMMAND sleep 1
     COMMAND touch ${CUDD_ROOT}/aclocal.m4
     COMMAND sleep 1
     COMMAND touch ${CUDD_ROOT}/configure ${CUDD_ROOT}/config.h.in ${CUDD_ROOT}/Makefile.in
+    CONFIGURE_HANDLED_BY_BUILD true
     CONFIGURE_COMMAND
         ${CUDD_ROOT}/configure
         --with-pic=yes
         --prefix=${CUDD_INSTALL_DIR}
-        CC=${CMAKE_C_COMPILER}
-        CXX=${CMAKE_CXX_COMPILER}
-        CFLAGS=${CMAKE_C_FLAGS}
-        CXXFLAGS=${CMAKE_CXX_FLAGS}
-    BUILD_COMMAND make -j1
-    INSTALL_COMMAND make -j1 install
+    BUILD_COMMAND ${MAKE_EXECUTABLE} -j1
+    INSTALL_COMMAND ${MAKE_EXECUTABLE} install
     COMMAND cp ${CUDD_ROOT}/cudd/cuddInt.h ${CUDD_INSTALL_DIR}/include
     COMMAND cp ${CUDD_ROOT}/st/st.h ${CUDD_INSTALL_DIR}/include
     COMMAND cp ${CUDD_ROOT}/mtr/mtr.h ${CUDD_INSTALL_DIR}/include
     COMMAND cp ${CUDD_ROOT}/epd/epd.h ${CUDD_INSTALL_DIR}/include
     COMMAND cp ${CUDD_ROOT}/util/util.h ${CUDD_INSTALL_DIR}/include
-    COMMAND cp ${CUDD_ROOT}/config.h ${CUDD_INSTALL_DIR}/include # generated header
-    BUILD_IN_SOURCE ON
-    LOG_CONFIGURE ON
-    LOG_BUILD ON
-    LOG_INSTALL ON
-    LOG_OUTPUT_ON_FAILURE ON
+    COMMAND cp ${CUDD_BINARY_DIR}/config.h ${CUDD_INSTALL_DIR}/include # generated header
+    BUILD_IN_SOURCE OFF
+    BUILD_BYPRODUCTS ${CUDD_LIB}
 )
 
 # Workaround https://gitlab.kitware.com/cmake/cmake/-/issues/15052
@@ -37,7 +37,7 @@ file(MAKE_DIRECTORY "${CUDD_INSTALL_DIR}/include")
 
 add_library(cudd STATIC IMPORTED GLOBAL)
 set_target_properties(cudd PROPERTIES
-    IMPORTED_LOCATION "${CUDD_INSTALL_DIR}/lib/libcudd.a"
+    IMPORTED_LOCATION ${CUDD_LIB}
     INTERFACE_INCLUDE_DIRECTORIES "${CUDD_INSTALL_DIR}/include"
 )
 add_dependencies(cudd cudd_src)
